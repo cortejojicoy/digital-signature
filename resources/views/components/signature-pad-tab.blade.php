5cd304cb0690a6a802f0f4352671f3db191d6ef5
cortejojicoy <johnjercai.cortejo@lspu.edu.ph>
2026-04-28 14:59:22 +0800

    refactor: update styling of signature pad component for improved appearance

--- resources/views/components/signature-pad-tab.blade.php
+++ resources/views/components/signature-pad-tab.blade.php
@@ -1,9 +1,7 @@
-<div class="rounded-xl border border-gray-200 dark:border-white/10
-            bg-white dark:bg-white/5 overflow-hidden">
+<div class="rounded-lg bg-gray-50/60 dark:bg-white/[0.03] overflow-hidden">
 
     {{-- Header hint --}}
-    <div class="flex items-center gap-2 border-b border-gray-100 dark:border-white/[0.06]
-                px-4 py-2.5 bg-gray-50/60 dark:bg-white/[0.03]">
+    <div class="flex items-center gap-2 px-3 py-2">
         <svg class="h-3.5 w-3.5 text-gray-400 shrink-0" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" clip-rule="evenodd"
                   d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1
@@ -11,8 +9,8 @@
                      l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253
                      a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"/>
         </svg>
-        <span class="text-xs text-gray-400 dark:text-gray-500">
-            Draw your signature, then click <strong class="font-medium text-gray-500 dark:text-gray-400">{{ $field->getConfirmLabel() }}</strong>
+        <span class="text-xs text-gray-500 dark:text-gray-400">
+            Draw your signature, then click <strong class="font-semibold text-gray-700 dark:text-gray-200">{{ $field->getConfirmLabel() }}</strong>
         </span>
     </div>
 
