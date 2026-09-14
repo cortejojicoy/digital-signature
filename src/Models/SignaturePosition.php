b3d5f8bc0d500832fb8aa07d5e137874deacb0e0
cortejojicoy <johnjercai.cortejo@lspu.edu.ph>
2026-09-14 13:40:21 +0800

    feat: Add caption_position to fillable attributes in SignaturePosition model

--- src/Models/SignaturePosition.php
+++ src/Models/SignaturePosition.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class SignaturePosition extends Model
 {
-    protected $fillable = ['signature_id', 'page', 'x', 'y', 'width', 'height', 'label'];
+    protected $fillable = [
+        'signature_id', 'page', 'x', 'y', 'width', 'height', 'label',
+        // Which side of this stamp the provenance caption sits on. Null means
+        // "whatever the application is configured for".
+        'caption_position',
+    ];
 
     public function signature(): BelongsTo
     {
